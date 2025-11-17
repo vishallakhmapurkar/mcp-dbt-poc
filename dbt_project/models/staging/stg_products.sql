1eaa54a1c288e112267319984a69f0e928284bc5
vishallakhmapurkar <lakhmapurkar.vishal@gmail.com>
2025-11-17 08:41:57 +0100

    Add dbt models

---
+++ dbt_project/models/staging/stg_products.sql
@@ -0,0 +1,8 @@
+{{ config(materialized='view') }}
+
+SELECT
+    product_id AS product_id,
+    product_name AS product_name,
+    category AS category,
+    price AS price
+FROM {{ source('dbt_vlakhmapurkar', 'products') }}
